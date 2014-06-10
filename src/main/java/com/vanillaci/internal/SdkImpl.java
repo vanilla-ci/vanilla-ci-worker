4245b8c203cf2a2e3127390f731b4021cb3b0777
Joel Johnson <joelj@joelj.com>
2014-06-09 22:05:10 -0600

    getting parameters to be populated correctly

--- src/main/java/com/vanillaci/internal/SdkImpl.java
+++ src/main/java/com/vanillaci/internal/SdkImpl.java
@@ -1,7 +1,9 @@
 package com.vanillaci.internal;
 
+import com.vanillaci.plugins.*;
+
 /**
  * @author Joel Johnson
  */
-public class SdkImpl {
+public class SdkImpl implements Sdk{
 }
--- src/main/java/com/vanillaci/internal/services/WorkService.java
+++ src/main/java/com/vanillaci/internal/services/WorkService.java
@@ -1,9 +1,14 @@
 package com.vanillaci.internal.services;
 
+import com.google.common.collect.*;
+import com.vanillaci.internal.*;
 import com.vanillaci.internal.exceptions.*;
 import com.vanillaci.internal.model.*;
 import com.vanillaci.plugins.*;
 import org.apache.logging.log4j.*;
+
+import java.io.*;
+import java.util.*;
 
 /**
  * @author Joel Johnson
@@ -17,11 +22,14 @@ public class WorkService {
 		this.buildStepService = buildStepService;
 	}
 
-	public BuildStep.Result executeWork(Work work) {
-		BuildStepContext buildStepContext = null;
+	public BuildStep.Result executeWork(Work work) throws IOException {
+		File workspace = new File("workspace");
+		if(!workspace.exists() && workspace.mkdirs()) {
+			throw new IOException("couldn't create workspace " + workspace.getAbsolutePath());
+		}
 
-		BuildStep.Result finalResult = executeBuildSteps(work, buildStepContext);
-		BuildStep.Result postBuildFinalResult = executePostBuildSteps(work, buildStepContext, finalResult);
+		BuildStep.Result finalResult = executeBuildSteps(work, workspace);
+		BuildStep.Result postBuildFinalResult = executePostBuildSteps(work, workspace, finalResult);
 
 		if(postBuildFinalResult.isWorseThan(finalResult)) {
 			finalResult = postBuildFinalResult;
@@ -30,7 +38,7 @@ public class WorkService {
 		return finalResult;
 	}
 
-	private BuildStep.Result executeBuildSteps(Work work, BuildStepContext buildStepContext) {
+	private BuildStep.Result executeBuildSteps(Work work, File workspace) {
 		BuildStep.Result finalResult = BuildStep.Result.SUCCESS;
 		try {
 			for (BuildStepMessage buildStepMessage : work.getBuildSteps()) {
@@ -38,6 +46,15 @@ public class WorkService {
 				if(buildStep == null) {
 					throw new UnresolvedBuildStepException(buildStepMessage);
 				}
+
+				Map<String, String> buildStepParameters = buildStepMessage.getParameters();
+				Map<String, String> workParametersParameters = work.getParameters();
+				Map<String, String> allParameters = ImmutableMap.<String, String>builder()
+					.putAll(buildStepParameters)
+					.putAll(workParametersParameters)
+					.build();
+
+				BuildStepContext buildStepContext = new BuildStepContextImpl(allParameters, workspace, new SdkImpl());
 
 				BuildStep.Result result = buildStep.execute(buildStepContext);
 
@@ -59,12 +76,21 @@ public class WorkService {
 		return finalResult;
 	}
 
-	private BuildStep.Result executePostBuildSteps(Work work, BuildStepContext buildStepContext, BuildStep.Result finalResult) {
+	private BuildStep.Result executePostBuildSteps(Work work, File workspace, BuildStep.Result finalResult) {
 		for (BuildStepMessage buildStepMessage : work.getPostBuildSteps()) {
 			BuildStep buildStep = buildStepService.get(buildStepMessage.getName(), buildStepMessage.getVersion());
 			if(buildStep == null) {
 				throw new UnresolvedBuildStepException(buildStepMessage);
 			}
+
+			Map<String, String> buildStepParameters = buildStepMessage.getParameters();
+			Map<String, String> workParametersParameters = work.getParameters();
+			Map<String, String> allParameters = ImmutableMap.<String, String>builder()
+				.putAll(buildStepParameters)
+				.putAll(workParametersParameters)
+				.build();
+
+			BuildStepContext buildStepContext = new BuildStepContextImpl(allParameters, workspace, new SdkImpl());
 
 			try {
 				BuildStep.Result result = buildStep.execute(buildStepContext);
